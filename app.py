8fbcad3d77b56a92721b7ed2536f97e8f9ce1423
amanderwal <124696250+amanderwal@users.noreply.github.com>
2023-02-22 01:06:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+# Set up the OpenAI API credentials
+openai.api_key = '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'
+
+# Define the route for generating bot responses
+@app.route('/api/bot', methods=['POST'])
+def generate_response():
+    user_input = request.json['input']
+    # return jsonify({'response': 'message'})
+    model_engine = "text-davinci-003"
+    prompt = (f"{user_input}")
+    
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    # print(message)
+    return jsonify({'response': message})
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
